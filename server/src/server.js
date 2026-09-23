5038b7e5072923a24471ecb10403838d50827c08
SergiuSPT <96991196+SergiuSPT@users.noreply.github.com>
2026-09-23 10:07:45 +0300

    railway server binding

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
 import { app } from "./app.js";
 import { env } from "./config/env.js";
 
-app.listen(env.PORT, () => {
-  console.log(`Server running at http://localhost:${env.PORT}`);
+app.listen(env.PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${env.PORT}`);
 });
